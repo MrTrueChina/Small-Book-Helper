440a19c265f82fa509b0a9b993ee1db19d64a8cc
Mr.ture.China <mr.true.china@gmail.com>
2022-07-10 20:44:59 +0800

    分P重命名文件方法添加多种重命名方式

--- 子功能模块/重命名文件名为分P名称.py
+++ 子功能模块/重命名文件名为分P名称.py
@@ -2,6 +2,23 @@
 
 import os # 导入 os 模块
 
+# 选择重命名规则的提示
+print("请选择重命名方式：")
+print("（FP）重命名为 {文件夹名称}_p{页码}")
+print("（FN）重命名为 {文件夹名称}_{文件原名}")
+print("（FPN）重命名为 {文件夹名称}_p{页码}_{文件原名}")
+
+# 接收重命名规则
+renameRule = input("\n")
+
+# 将重命名规则改为小写
+renameRule = renameRule.lower()
+
+# 输入错误则发出提示后结束运行
+if(renameRule != "fp" and renameRule != "fn" and renameRule != "fpn"):
+    print("没有选择重命名方式，取消重命名")
+    exit()
+
 # 获取当前文件所处的文件夹的完整路径
 currentFloderPath = os.getcwd()
 
@@ -16,12 +33,23 @@ for i in range(len(allFiles)):
     # 重名前的完整名称
     oldFullName = allFiles[i]
     
-    # 后缀名
+    # 后缀名和文件名
+    fileName = oldFullName
     suffix = ""
     if (oldFullName.rfind(".") != -1):
+        fileName = oldFullName[ : oldFullName.rfind(".")]
         suffix = oldFullName[oldFullName.rfind(".") : ]
 
-    # 新的名称 = [当前文件夹名称]_p[i]
-    newName = currentFloderName + "_p" + str(i) + suffix
+    # FP（Folder Part/Page）：新的名称 = [当前文件夹名称]_p[i]
+    if(renameRule == 'fp'):
+        newName = currentFloderName + "_p" + str(i) + suffix
+
+    # FN（Folder Name）：新的名称 = [当前文件夹名称]_p[原始文件名]
+    if(renameRule == 'fn'):
+        newName = currentFloderName + "_" + fileName + suffix
+    
+    # FP（Folder Part/Page Name）：新的名称 = [当前文件夹名称]_p[i]_[原始文件名]
+    if(renameRule == 'fpn'):
+        newName = currentFloderName + "_p" + str(i) + "_" + fileName + suffix
 
     os.rename(allFiles[i], newName)
